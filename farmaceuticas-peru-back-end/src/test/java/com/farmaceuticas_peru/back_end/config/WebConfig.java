76372563864f1ecb2c73b0f55eaaf43172892ffa
Gabriel Rincon <gabriel.rincon.m@uni.pe>
2026-04-16 22:06:54 -0500

    commit Gartur

---
+++ farmaceuticas-peru-back-end/src/test/java/com/farmaceuticas_peru/back_end/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.farmaceuticas_peru.back_end.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permite que la app Angular (en localhost:4200) haga peticiones a la API.
+        registry.addMapping("/api/**") // Aplica a todas las rutas que empiecen con /api
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
